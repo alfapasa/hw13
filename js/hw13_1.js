ff9a9b25f464f79b98eb7a2afd20edc01e92762e
liza <elizavetakulik2@gmail.com>
2024-02-15 11:57:05 +0100

    commit2

--- js/hw13_1.js
+++ js/hw13_1.js
@@ -7,17 +7,21 @@ let arrNumbers = [];
 
 function showMedSumNumbers() {
 
+    let sum = 0;
+    let medSum = 1;
+
     for (let i = 0; i < arr.length; i++) {
 
-        if (typeof arr[i] === 'Number') {
-            arrNumbers.push(i);
+        if (typeof arr[i] === 'number') {
+            arrNumbers.push(arr[i]);
         }     
 
-        let medSum = 0;
-        medSum = (medSum + arrNumbers[i]) / arrNumbers.length;
-
-        return(medSum);
+        sum += arrNumbers[i]; //считаю тут сумму потому что i не будет видно за пределами цикла for;
     }
+
+    medSum = sum / arrNumbers.length;
+
+    return(medSum); // medSum возвращается в скобки функции
 }
 
 arr.push(1,1,1,1,1,'s','s','d');
